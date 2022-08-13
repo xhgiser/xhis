e45c9297ceecfe35deddf50fc6d6547b01aeeab9
lcx <771616466@qq.com>
2022-08-13 13:32:17 +0800

    路由基础配置,路由拦截

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,17 +1,37 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-// 路由规则
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/home/home')
-  }
-]
-
+//路由配置
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+  history: createWebHashHistory(), //选择hash路由
+  routes: [
+    /**
+     * 复制过来的,还没有配置
+     */
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      path: '/home',
+      component: () => import('../views/home/home.vue'),
+      meta: {
+        isAuth: true,
+      },
+    },
+  ],
+})
+
+//路由拦截
+router.beforeEach((to, from, next) => {
+  if (to.meta.isAuth) {
+    if (localStorage.getItem('token')) {
+      next()
+    } else {
+      ElMessage.error('请先登录')
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
